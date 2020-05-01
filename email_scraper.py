84215a228da7b83f7018a9b29f85fb7648319273
austincies <austincies@gmail.com>
2020-04-30 22:16:42 -0400

    Now follows pep8 standards for naming

--- email_scraper.py
+++ email_scraper.py
@@ -6,20 +6,20 @@ import urllib
 import sys
 
 
-class mailSpider:
+class MailSpider:
 
-    def __init__(self, scope, maxEmail):
+    def __init__(self, scope, max_emails):
         self.visited = set()
         self.to_visit = queue.Queue()
         scope = scope.strip("http://")
-        scope = urllib.parse.urlparse("//" + scope)[1] 
+        scope = urllib.parse.urlparse("//" + scope)[1]
         self.to_visit.put(scope)
         self.emails = set()
-        self.emailCount = 0
-        self.maxEmails = maxEmail
+        self.email_count = 0
+        self.max_emails = max_emails
         self.scope = scope
 
-    def linkValid(self, anchor):
+    def link_valid(self, anchor):
         if len(anchor) == 0:
             return False
         elif anchor[0] == "w" or anchor[0] == "h" or anchor[0] == "/":
@@ -27,7 +27,7 @@ class mailSpider:
         else:
             return False
 
-    def inScope(self, url):
+    def in_scope(self, url):
         if url == self.scope:
             return True
         try:
@@ -46,20 +46,20 @@ class mailSpider:
             u = urllib.parse.urlparse("https://" + url)
             response = requests.get("https://" + url)
 
-            newEmails = set(re.findall(
+            new_emails = set(re.findall(
                 r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
-            for email in newEmails:
-                if self.emailCount >= self.maxEmails:
+            for email in new_emails:
+                if self.email_count >= self.max_emails:
                     return
                 if email not in self.emails:
-                    self.emailCount = self.emailCount + 1
+                    self.email_count = self.email_count + 1
                     self.emails.add(email)
 
             soup = bs4.BeautifulSoup(response.text, 'html.parser')
             for newLink in soup.find_all('a', href=True):
                 try:
                     anchor = newLink['href']
-                    if self.linkValid(anchor) == False:
+                    if self.link_valid(anchor) == False:
                         next
                     else:
                         if anchor[0] == "/":
@@ -67,37 +67,36 @@ class mailSpider:
                         aUrl = urllib.parse.urlparse(anchor)
                         if aUrl[1] == '':
                             aUrl = urllib.parse.urlparse("//" + anchor)
-                        if self.inScope(aUrl[1]) and (aUrl[1] + aUrl[2]) not in self.visited:
+                        if self.in_scope(aUrl[1]) and (aUrl[1] + aUrl[2]) not in self.visited:
                             self.to_visit.put(aUrl[1] + aUrl[2])
                 except:
                     next
         except:
             pass
 
-
-    def saveEmails(self):
+    def save_emails(self):
         f = open("emails.txt", "a")
         for email in self.emails:
             f.write(email + "\n")
 
     def crawl(self):
         while True:
-            if self.emailCount >= self.maxEmails:
+            if self.email_count >= self.max_emails:
                 break
             elif self.to_visit.empty():
                 break
             else:
                 url = self.to_visit.get()
                 self.scrape(url)
-        self.saveEmails()
+        self.save_emails()
 
 
 def main():
     if len(sys.argv) != 3:
-        print("Usage: emailScraper.py [url], [emailCount]")
+        print("Usage: emailScraper.py [url], [email_count]")
     url = sys.argv[1]
-    maxEmail = sys.argv[2]
-    spider = mailSpider(url, int(maxEmail))
+    max_emails = sys.argv[2]
+    spider = MailSpider(url, int(max_emails))
     spider.crawl()
 
 
